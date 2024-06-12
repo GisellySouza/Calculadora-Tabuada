5a5c0bbc623e0575733d61ce2cd7778ac556047d
GisellySouza <gidesouza1234@gmail.com>
2024-06-12 11:01:51 -0300

    Finalizado

--- funcionalidade.js
+++ funcionalidade.js
@@ -1,20 +1,23 @@
 function Calcular(){
     var num = window.document.getElementById('num')
     var final = document.getElementById('final')
-   var n = Number(num.value)
-   var f = Number(final.value)
-   var x= 0
-   var r= ''
-   var resp = window.document.getElementById('resposta')
-   r= n*f
-   resp.innerHTML=`A tabuada da ${n} é:<br>`
-   
+    if (num.value.length == 0 || final.value.length == 0){
+        window.alert('[ERRO]: Preencha os dados.')
+    } else {
+        var n = Number(num.value)
+        var f = Number(final.value)
+        var x= 0
+        var r= ''
+        var resp = window.document.getElementById('resposta')
+        r= n*f
+        resp.innerHTML=`A tabuada da ${n} é:<br>`
+        
 
-   do{ 
-    r = x*n; 
-    resp.innerHTML +=`${x} X ${n} = ${r}<br>`
-    x++
-    
-   }while ( x <= f )
-
+        do{ 
+            r = x*n; 
+            resp.innerHTML +=`${x} X ${n} = ${r}<br>`
+            x++
+            
+        }while ( x <= f )
+   }
 }
